e50a1535d82913ad82f7d3ead681d545b50e925f
AsuraMzn <ribubaucha@gmail.com>
2024-06-13 21:15:29 +0545

    remove unnecessary lines

--- proj-front/medicine-display.php
+++ proj-front/medicine-display.php
@@ -60,12 +60,7 @@
         <?php include 'includes/dashboard.php'; ?>
         <div class="container-fluid p-4 bg-body-tertiary">
                     <?php
-                        // $medicine = getAll('user_medicine_tbl');
                         $medicine = getAllById('user_medicine_tbl', 'pharmacy_id', $user_id);
-                        // $query = "SELECT * FROM user_medicine_tbl";
-                        // $query ="SELECT * FROM user_medicine_tbl WHERE pharmacy_id='$user_id'";
-                        // $row = mysqli_query($conn, $query);
-
 
                         if($medicine['status'] == 200)
                         {
